c07b2f9fbf69c452faeb560c341639ae72c8cde2
Dan Griffiths <dg4978@outlook.com>
2025-08-22 13:41:27 +0200

    Initial commit: added test

--- cs50w/week3/wiki/encyclopedia/tests.py
+++ cs50w/week3/wiki/encyclopedia/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class TestRoutes(TestCase):
+
+    def test_index_view_status_200(self):
+
+        # create a url to page to be tested. use reverse() method with "app_name:view_name"
+        url = reverse("encyclopedia:index")
+        # visit url using client.get(url), this is a class so need to use self.client.get(url)
+        response = self.client.get(url)
+
+
+        # test for status code 200, and a word shown on html page
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Encyclopedia")
+
+
+
+
+ 
+ 
