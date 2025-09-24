63ca9f7a63993d9d80dbd48ba5fe61402bb89baf
Dan Griffiths <dg4978@outlook.com>
2025-09-24 16:12:26 +0200

    housekeeping files wiki project

--- cs50w/week3/wiki/wiki/settings.py
+++ cs50w/week3/wiki/wiki/settings.py
@@ -122,7 +122,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 
 # Default primary key field type
--- cs50w/week3/wiki/wiki/urls.py
+++ cs50w/week3/wiki/wiki/urls.py
@@ -17,8 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+#######
+# for media
+from django.conf import settings
+from django.conf.urls.static import static
+#######
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('encyclopedia/', include("encyclopedia.urls")),
 
 ]
+
+
+# media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
